2ee8cacba3c3db285cbdee9d9d3271f68cd34799
LukasBarry <Lukasbarry@yahoo.com>
2017-02-16 15:12:48 -0500

    refactors tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -62,4 +62,6 @@ RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::ControllerHelpers, type: :view
   config.include FactoryGirl::Syntax::Methods
+
+  config.include Request::JsonHelpers, :type => :controller
 end
