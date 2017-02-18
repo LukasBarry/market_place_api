9f1f72015bebb91b31e4eaa999b60263e3f93000
LukasBarry <Lukasbarry@yahoo.com>
2017-02-18 12:51:33 -0500

    refactors tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -64,4 +64,9 @@ RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
 
   config.include Request::JsonHelpers, :type => :controller
+  config.include Request::HeadersHelpers, :type => :controller
+
+  config.before(:each, type: :controller) do
+    include_default_accept_headers
+  end
 end
